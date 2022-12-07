3f352bd3f8f488f54fe842343af4cc179c978967
Chris Sayer <crichardsayer@gmail.com>
2022-12-07 15:18:53 -0500

    version 4

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+// NASA API
+
+const count = 10;
+const apiKey = 'DEMO_KEY';
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+
+// get 10 images from NASA API
+async function getNasaPictures() {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+    console.log(resultsArray);
+  } catch (error) {
+    // catch error here
+  }
+}
+
+//  On load
+getNasaPictures();
